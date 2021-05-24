89ac2147b0b18157c8ffeee4fc2a2f7c3abed57a
cubest_june <cubest_june@cuber-juneui-MacBookAir.local>
2021-05-24 23:16:22 +0900

    Add bootstrap navbar

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include # new
 from . import views
 
 urlpatterns = [
-    path('math_note/', include('math_note.urls')),
-    path('english_note/', include('english_note.urls')),
+    path('math_note/', include('math_note.urls'), name='math-note'),
+    path('english_note/', include('english_note.urls'), name='english-note'),
     path('', views.home_page)
 ]
