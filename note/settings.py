8c0ef0a8495a9fec7267e8ffa2d0bbd2ff972490
cubest_june <cubest_june@cuber-juneui-MacBookAir.local>
2021-05-25 15:51:22 +0900

    Add navbor

--- note/settings.py
+++ note/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-TEMPLATES_ROOT = os.path.join(BASE_DIR, 'templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
+
 
 
 MEDIA_URL = '/media/'
